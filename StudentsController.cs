6bd2d811c47f5c52f7afc1cc22b86865d9e1ce16
yahaya11267-ai <yahaya11267@bazeuniversity.edu.ng>
2026-04-09 10:41:22 +0100

    Update StudentsController.cs

---
+++ StudentsController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using StudentAPI.Data;
+using StudentAPI.Models;
+
+namespace StudentAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public StudentsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+            return Ok(_context.Students.ToList());
+        }
+
+        [HttpPost]
+        public IActionResult AddStudent(Student student)
+        {
+            _context.Students.Add(student);
+            _context.SaveChanges();
+            return Ok(student);
+        }
+    }
+}
