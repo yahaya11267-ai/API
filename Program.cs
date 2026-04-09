3eacb1700006affc3f3ef192fdb1dcecf4f031a0
yahaya11267-ai <yahaya11267@bazeuniversity.edu.ng>
2026-04-09 10:40:43 +0100

    Update Program.cs

---
+++ Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using StudentAPI.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
